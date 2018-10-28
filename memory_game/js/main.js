035e57e838aa92b9c92122b6f558228907a5906d
Edward Lea <edwardlea@Edwards-MBP.home>
2018-10-28 15:47:59 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,19 +2,24 @@
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if(cardsInPlay.length === 2){
+var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You've found a match!");
 	} else{
 		alert("Sorry, try again.")
 	}
 }
+
+
+var flipCard = function(cardID){
+console.log("User flipped " + cards[cardID])
+cardsInPlay.push(cards[cardID]);
+
+if(cardsInPlay.length === 2){
+	checkForMatch();
+}
+}
+
+flipCard(0);
+flipCard(2);
