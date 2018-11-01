cb3b933b458c7df972ff7cc3106e0d5b8f4c18ae
Edward Lea <edwardlea@Edwards-MBP.home>
2018-11-01 09:07:05 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,6 @@
 
 
-var cards = [
+var cardDeck = [
 {
 rank: "queen",
 suit: "hearts",
@@ -23,10 +23,15 @@ cardImage: "images/king-of-diamonds.png"
 }
 ];
 
+var cards = cardDeck.sort(function(a,b){return 0.5 - Math.random()});
+
 var cardsInPlay = [];
 
+var resultsArray = [];
 
+var score = 0;
 
+console.log(score);
 
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
@@ -34,17 +39,22 @@ var checkForMatch = function(){
 	} else{
 		alert("Sorry, try again.")
 	}
+	var resetPrompt = document.getElementsByClassName('resetPrompt')[0];
+	resetPrompt.setAttribute('class', "resetPromptShow");
 }
 
 var flipCard = function(){
 	var cardId = this.getAttribute('data-id')
-console.log("User flipped " + cards[cardId].rank)
+console.log("User flipped " + cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 cardsInPlay.push(cards[cardId].rank);
 this.setAttribute('src', cards[cardId].cardImage);
 if(cardsInPlay.length === 2){
 	checkForMatch();
+	resultsArray.unshift("User flipped " + cardsInPlay[0] +" and " + cardsInPlay[1]);
+}else if (cardsInPlay.length > 2) {
+	resetBoard();
 }
 }
 
@@ -58,5 +68,44 @@ var createBoard = function(){
 	}
 }
 
+var recordResult = function(){
+	console.log(resultsArray);
+	var existingResults = document.getElementById("results");
+	while (existingResults.firstChild) {
+    existingResults.removeChild(existingResults.firstChild);
+}
+
+	for (var i = 0; i < resultsArray.length; i++){
+		var newResult = document.createElement('p');
+		var resultText = document.createTextNode(i+1 +". " + resultsArray[i]);
+		newResult.appendChild(resultText);
+		
+		document.getElementById('results').appendChild(newResult);
+		newResult.setAttribute('class', "resultItem");
+}
+
+var latestResult = document.getElementsByClassName('resultItem')[0];
+latestResult.setAttribute('class', "latestResult");
+}
+
 createBoard();
+
+var resetBoard = function(){
+	recordResult();
+
+for (var i = 0; i < cards.length; i++){
+		var cardReset = document.getElementsByTagName('img')[i];
+		cardReset.setAttribute('src', "images/back.png");
+
+}
+cards = cardDeck.sort(function(a,b){return 0.5 - Math.random()});
+cardsInPlay = [];
+
+var resetPromptRemove = document.getElementsByClassName('resetPromptShow')[0];
+	resetPromptRemove.setAttribute('class', "resetPrompt");
+
+}
+	
+
+
 
