f763c62d7e4a8a6547b450a056a1511f658fe504
Edward Lea <edwardlea@Edwards-MBP.home>
2018-11-08 17:03:51 +0000

    update to game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,37 +1,33 @@
 
 
-var cardDeck = [
-{
-rank: "queen",
-suit: "hearts",
-cardImage: "images/queen-of-hearts.png"
-},
-{
-rank: "queen",
-suit: "diamonds",
-cardImage: "images/queen-of-diamonds.png"
-},
-{
-rank: "king",
-suit: "hearts",
-cardImage: "images/king-of-hearts.png"
-},
-{
-rank: "king",
-suit: "diamonds",
-cardImage: "images/king-of-diamonds.png"
-}
+var cardDeck = [{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
 ];
+
 // Provides some random sorting of original card deck.
 var cards = cardDeck.sort(function(a,b){return 0.5 - Math.random()});
 
 var cardsInPlay = [];
-
 var resultsArray = [];
-
 var score = 0;
-
-
 
 // Checks to see if two selected cards match. Shows reset prompt once check has taken place.
 var checkForMatch = function(){
@@ -51,16 +47,16 @@ var checkForMatch = function(){
 // Flips cards on selection. calls checkForMatch() once two cards have been selected. resetBoard() called once third card selected.
 var flipCard = function(){
 	var cardId = this.getAttribute('data-id')
-console.log("User flipped " + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-cardsInPlay.push(cards[cardId].rank);
-this.setAttribute('src', cards[cardId].cardImage);
-if(cardsInPlay.length === 2){
-	checkForMatch();
-	resultsArray.unshift("User flipped " + cardsInPlay[0] +" and " + cardsInPlay[1]);
-}else if (cardsInPlay.length > 2) {
-	resetBoard();
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if(cardsInPlay.length === 2){
+		checkForMatch();
+		rankesultsArray.unshift("User flipped " + cardsInPlay[0] +" and " + cardsInPlay[1]);
+	}else if (cardsInPlay.length > 2) {
+		resetBoard();
 }
 }
 
@@ -80,32 +76,31 @@ var recordResult = function(){
 	console.log(resultsArray);
 	var existingResults = document.getElementById("results");
 	while (existingResults.firstChild) {
-    existingResults.removeChild(existingResults.firstChild);
-}
+    	existingResults.removeChild(existingResults.firstChild);
+	}
 	for (var i = 0; i < resultsArray.length; i++){
 		var newResult = document.createElement('p');
 		var resultText = document.createTextNode(i+1 +". " + resultsArray[i]);
 		newResult.appendChild(resultText);
-		
 		document.getElementById('results').appendChild(newResult);
 		newResult.setAttribute('class', "resultItem");
-}
-var latestResult = document.getElementsByClassName('resultItem')[0];
-latestResult.setAttribute('class', "latestResult");
+	}
+	var latestResult = document.getElementsByClassName('resultItem')[0];
+	latestResult.setAttribute('class', "latestResult");
 }
 
 createBoard();
 
-// Call recordResults() to capture results, resets board, showing back of cards and randomises array. 
+// Call recordResults() to capture results, resets board, showing back of cards and randomise cards array. 
 var resetBoard = function(){
 	recordResult();
-for (var i = 0; i < cards.length; i++){
+	for (var i = 0; i < cards.length; i++){
 		var cardReset = document.getElementsByTagName('img')[i];
 		cardReset.setAttribute('src', "images/back.png");
-}
-cards = cardDeck.sort(function(a,b){return 0.5 - Math.random()});
-cardsInPlay = [];
-var resetPromptRemove = document.getElementsByClassName('resetPromptShow')[0];
+	}
+	cards = cardDeck.sort(function(a,b){return 0.5 - Math.random()});
+	cardsInPlay = [];
+	var resetPromptRemove = document.getElementsByClassName('resetPromptShow')[0];
 	resetPromptRemove.setAttribute('class', "resetPrompt");
 }
 	
